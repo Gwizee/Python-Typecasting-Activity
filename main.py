a671f75eaeb7256349392352190b7e1e0fbea3e3
Gwizee <risingfire69@gmail.com>
2024-09-30 21:35:23 +0600

    Python Typecasting Activity is completed

---
+++ main.py
@@ -0,0 +1,17 @@
+age=14
+print("Data Type of age:",type(age))
+#integer to float Typecasting
+age=float(age)
+print("Data Type of age:",type(age))
+
+weight=84.5
+print("Data Type of weight:",type(weight))
+#float to boolean Typecasting
+weight=str(weight)
+print("Data Type of weight:",type(weight))
+
+height=5.7
+print("Data Type of height:",type(height))
+#float to integer Typecasting
+height=int(height)
+print("Data Type of height:",type(height))
